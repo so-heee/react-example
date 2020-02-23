f0a4b7635488100ed57c27b03bfa4d9244677b2b
so-heee <so.heee.mochi@gmail.com>
2020-02-23 13:41:07 +0900

    apply query and variables

--- pure-react-example/tutorial/udemy/udemy-graphql-with-react-intro-search-repos/src/App.js
+++ pure-react-example/tutorial/udemy/udemy-graphql-with-react-intro-search-repos/src/App.js
@@ -3,19 +3,33 @@ import { ApolloProvider } from 'react-apollo'
 import{ Query } from 'react-apollo'
 import Client from './client'
 import { ME } from './graphql'
+import { SEARCH_REPOSITORYS } from './graphql'
 
+const VARIABLES = {
+  first: 5,
+  after: null,
+  before: null,
+  last: null,
+  query: "フロントエンドエンジニア"
+}
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = VARIABLES
+  }
   render() {
+    const { query, first, last, before, after } = this.state
     return (
       <ApolloProvider client={Client}>
-        <div>Hello, GraphQL</div>
-        <Query query={ME}>
+        <Query 
+          query={SEARCH_REPOSITORYS}
+          variables={{ query, first, last, before, after}}>
           {
             ({ loading, error, data}) => {
               if (loading) return 'Loading...'
               if (error) return `Error!: ${error.message}`
-
-              return <div>{data.user.name}</div>
+              console.log(data)
+              return <div></div>
             }
           }
         </Query>
--- pure-react-example/tutorial/udemy/udemy-graphql-with-react-intro-search-repos/src/graphql.js
+++ pure-react-example/tutorial/udemy/udemy-graphql-with-react-intro-search-repos/src/graphql.js
@@ -1,5 +1,33 @@
 import gql from 'graphql-tag'
 
+export const SEARCH_REPOSITORYS = gql`
+    query searchRepositorys($first: Int, $after: String, $last: Int, $before: String, $query: String!) {
+        search(first: $first, after: $after, before: $before, last: $last, query: $query, type: REPOSITORY) {
+            repositoryCount
+            pageInfo {
+            endCursor
+            hasNextPage
+            hasPreviousPage
+            startCursor
+            }
+            edges{
+            cursor
+            node {
+                ... on Repository {
+                id
+                name
+                url
+                stargazers {
+                    totalCount
+                }
+                viewerHasStarred
+                }
+            }
+            }
+        }
+    }
+`
+
 export const ME = gql`
   query me { 
     user(login: "so-heee") { 
@@ -8,3 +36,4 @@ export const ME = gql`
     }
   }
 `
+
