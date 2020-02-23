aff2057691cfb38a4078ded323ac47a8504c322f
so-heee <so.heee.mochi@gmail.com>
2020-02-23 15:48:06 +0900

    show viewer has starred

--- pure-react-example/tutorial/udemy/udemy-graphql-with-react-intro-search-repos/src/App.js
+++ pure-react-example/tutorial/udemy/udemy-graphql-with-react-intro-search-repos/src/App.js
@@ -5,8 +5,15 @@ import Client from './client'
 import { SEARCH_REPOSITORYS } from './graphql'
 
 const StarButton = props => {
-  const totalCount = props.node.stargazers.totalCount
-return <button>{totalCount === 1 ? "1 star" : `${totalCount} stars`}</button>
+  const node = props.node
+  const totalCount = node.stargazers.totalCount
+  const viewerHasStarred = node.viewerHasStarred
+  const starCount = totalCount === 1 ? "1 star" : `${totalCount} stars`
+  return (
+    <button>
+      {starCount} | {viewerHasStarred ? 'stared' : '-'}
+    </button>
+  )
 }
 
 const PER_PAGE = 5
